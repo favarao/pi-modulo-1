dc34058b03181b5e3cb589e1c2260037fc6d8d94
João Pedro <favarao_vieira@hotmail.com>
2024-08-20 02:16:13 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import session from 'express-session';
+import { autenticar, verificaSessao, sair } from './seguranca/autenticar.js';
+
+
+const host = '0.0.0.0';
+const porta = 3000;
+const app = express();
+
+//a biblioteca qs será utilizada para tratar os parâmetros da requisição
+app.use(express.urlencoded({ extended: true }));
+
+//aqui está sendo configurado o uso da biblioteca session
+app.use(session({
+    secret: 'unoeste', //chave para criptografia
+    resave: true,
+    saveUninitialized: true,
+    cookie: {  
+        maxAge: 1000 * 60 * 1 // 1 minuto
+    }
+}));
+// quando buscas /node_modules, ele vai buscar na pasta node_modules
+app.use('/node_modules', express.static('node_modules'));
+app.use(express.static('./public'));
+
+app.get('/', (requisicao, resposta) => {
+    resposta.redirect('/eventos.html');
+});
+
+app.post('/login', autenticar);
+
+app.get('/sair', sair);
+
+app.use(verificaSessao, express.static('./private'));
+
+
+app.listen(porta, host, () => {
+    console.log(`Servidor rodando em http://${host}:${porta}`);
+});
+
---
+++ seguranca/autenticar.js
@@ -0,0 +1,35 @@
+export function autenticar(requisicao, resposta){
+    const usuario = requisicao.body.usuario;
+    const senha   = requisicao.body.senha;
+    if (usuario == 'joao' && senha == 'fipp') {
+        requisicao.session.autenticado = true;
+        resposta.redirect('/eventos.html');
+    }
+    else
+    {
+        resposta.write('<html>');
+        resposta.write('<head>');
+        resposta.write('<title>Falha no login</title>');
+        resposta.write('<meta charset="utf-8">');
+        resposta.write('</head>');
+        resposta.write('<body>');
+        resposta.write('<p>Usuário ou senha inválidos</p>');
+        resposta.write('<a href="/login.html">Voltar para tela de login</a>');
+        resposta.write('</body>');
+        resposta.write('</html>');
+        resposta.end();
+    }
+}
+
+export function verificaSessao(requisicao, resposta, next){
+    if (requisicao.session.autenticado){
+        next();
+    } else {
+        resposta.redirect('/login.html');
+    }
+}
+
+export function sair(requisicao, resposta){
+    requisicao.session.destroy();
+    resposta.redirect('/login.html');
+}
