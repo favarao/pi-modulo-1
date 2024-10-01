84690a8f6ddbaae263debf07db700c8d185eb08c
Joao <favaraovieira@gmail.com>
2024-10-01 14:19:08 -0300

    cadastro de eventos

--- DAO/Conexao.js
+++ DAO/Conexao.js
@@ -13,7 +13,6 @@ export default async function conectar(){
             password: '',
             database: 'pieventos',
             waitForConnections: true,
-            connectionLimit: 10,
             maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
             idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
             queueLimit: 0,
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import session from 'express-session';
 import { autenticar, verificaSessao, sair } from './seguranca/autenticar.js';
-import Evento from "./Model/Evento.js";
-
 
 const host = '0.0.0.0';
 const porta = 3000;
@@ -17,7 +15,7 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
     cookie: {  
-        maxAge: 1000 * 60 * 2 // 2 minutos
+        maxAge: 1000 * 60 * 10 // 2 minutos
     }
 }));
 // quando buscas /node_modules, ele vai buscar na pasta node_modules
@@ -25,50 +23,7 @@ app.use('/node_modules', express.static('./node_modules'));
 app.use(express.static('./public'));
 
 app.get('/', (requisicao, resposta) => {
-    resposta.redirect('/eventos.html');
-});
-
-app.post('/evento', (requisicao, resposta) => {
-    
-    const id = requisicao.body.id;
-    const nome = requisicao.body.nome;
-    const cidade = requisicao.body.cidade;
-    const endereco = requisicao.body.endereco;
-    const data = requisicao.body.data;
-    const descricao = requisicao.body.descricao;
-    const valor = requisicao.body.valor;
-    const quantidade = requisicao.body.quantidade;
-    const status = 1;
-    const evento = new Evento(id, nome, cidade, endereco, data, descricao, valor, quantidade, status);
-    if(id)
-        evento.alterar();
-    else
-        evento.incluir();
-});
-
-app.get('/deleteEvento/:id',async (requisicao, resposta) => {
-    const id = requisicao.params.id;
-    const evento = new Evento();
-    evento.excluir(id);
     resposta.redirect('/evento-admin.html');
-});
-
-app.get('/evento/:id',async (requisicao, resposta) => {
-    const id = requisicao.params.id;
-    const evento = new Evento();
-    const eventos = await evento.consultar(id);
-    resposta.json(eventos);
-});
-
-app.get('/eventos', async  (requisicao, resposta) => {
-    // Criando uma instância da classe Evento
-    const evento = new Evento();
-
-    // Chama o método consultar() da instância de Evento
-    const eventos = await evento.consultar();
-
-    // Envia a lista de eventos como resposta em formato JSON
-    resposta.json(eventos);
 });
 
 app.post('/login', autenticar);
